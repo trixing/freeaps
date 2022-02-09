1c887ce20da738d4dc6e874d9c7193d6b59d05f4
Robert <comps@diprobe.com>
2022-02-09 16:21:52 +0100

    Icicle Basals & Colors
    tag color

--- FreeAPS/Sources/Views/TagCloudView.swift
+++ FreeAPS/Sources/Views/TagCloudView.swift
@@ -66,7 +66,7 @@ struct TagCloudView: View {
                 case "Standard":
                     return .loopYellow
                 default:
-                    return .tempBasal
+                    return .basal
                 }
             }
         }
