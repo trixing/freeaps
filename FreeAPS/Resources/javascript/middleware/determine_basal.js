74ca5a3cb96cec81204bbeac6f0341f22b68ace7
Jan Dittmer <jdi@l4x.org>
2022-03-03 21:23:49 +0100

    Simple middleware based on Chris formula without tdd calculation

--- FreeAPS/Resources/javascript/middleware/determine_basal.js
+++ FreeAPS/Resources/javascript/middleware/determine_basal.js
@@ -1,5 +1,27 @@
 function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock) {
-    // modify anything
-    // return any reason what has changed.
-    return "Nothing changed";
+    const adjustmentFactor = 1.0;
+    const TDD = 0;
+    if (!TDD || !adjustmentFactor) {
+        return "Middleware disabled";
+    }
+
+    // Should not need configuring below.
+    const bg = glucose[0].glucose;
+
+    if (!bg) {
+        return "Middleware, No blood glucose";
+    }
+    if (profile.min_bg > 115) {
+        return "Middleware, High target set";
+    }
+    var newISF = 277700 / (adjustmentFactor  * TDD * bg);
+    var newRatio = profile.sens / newISF;
+    if (newRatio < profile.autosens_min) {
+        newRatio = profile.autosens_min;
+    }
+    if (newRatio > profile.autosens_max) {
+        newRatio = profile.autosens_max;
+    }
+    autosens.ratio = newRatio;
+    return "Middleware, adjusting ISF to " + newISF + " with ratio " + newRatio;
 }
